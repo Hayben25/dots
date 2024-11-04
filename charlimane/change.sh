d353db19c290feb5192e9a923ccf8bb09094c786
Hayben25 <121698839+Hayben25@users.noreply.github.com>
2024-11-04 11:49:14 -0600

    Add files via upload

---
+++ charlimane/change.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Greetings mortal, I see you want to change your theme colors\n"
+
+echo "Which color would you like to change to(black, blue, green, grey, light-blue, light-green, orange, pink, purple, red, turquoise, white, yellow)"
+read -p "Choose:: " p
+
+cp ~/.config/charlemagne/$p ~/.config/i3/config
+
+read -p "Choose aspect ratio(16_9 or 16_10):: " r
+
+nitrogen --set-auto ~/backgrounds/$r/$p.png

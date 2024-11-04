56ab9e86898425b392fd23f0cd4c675ebea27624
Hayben25 <121698839+Hayben25@users.noreply.github.com>
2024-11-04 09:03:10 -0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mv ~/chrome ~/.mozilla/firefox/*e
+echo "remember to go to about:config and change the toolkit.legacyUserProfileCustomizations.stylesheets to true"
+
+mv config ~/.config/i3
+
+mv charlemagne .config
+
+sudo pacman -S kitty neofetch unzip net-tools
+
+bash <(curl https://end-4.github.io/dots-hyprland-wiki/setup.sh

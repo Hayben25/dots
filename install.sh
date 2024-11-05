ad609bdae6d3e8722948215a694f06c687a1fa9a
Hayben25 <hayden@cianessi.com>
2024-11-04 21:25:31 -0600

    hi

--- install.sh
+++ install.sh
@@ -17,4 +17,22 @@ spotify-launcher
 
 curl -fsSL https://raw.githubusercontent.com/spicetify/cli/main/install.sh | sh
 
+cd ~
+
+mkdir code
+
+cd code
+
+mkdir dots
+
+git clone https://gist.github.com/208fe1accd2219507b07a5e250a91df0.git
+
+cd dots
+
+git clone https://github.com/Hayben25/dots.git
+
+chmod +x ~/code/dots/update.sh
+
+./update.sh
+
 bash <(curl https://end-4.github.io/dots-hyprland-wiki/setup.sh)

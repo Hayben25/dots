c067286b34b7d2b91162c9885a7813a66016c509
Hayben25 <hayden@cianessi.com>
2024-11-04 20:06:17 -0600

    update

---
+++ update.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+rm -r ~/.config/polybar
+rm -r ~/.config/charlimane
+
+cp polybar ~/.config
+cp charlimane ~/.config
+
+cp config ~/.config/i3
+
+rm -r ~/backgrounds
+
+cp backgrounds ~
+
+cp .bashrc ~
+
+rm -r ~/.mozilla/firefox/*e/chrome
+cp -r ~/.mozilla/firefox/*e/chrome

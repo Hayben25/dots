7bd56667aae753696f191083539f26e4ea1688a8
Hayben25 <hayden@cianessi.com>
2024-11-04 20:09:38 -0600

    fix

--- update.sh
+++ update.sh
@@ -3,18 +3,18 @@
 rm -r ~/.config/polybar
 rm -r ~/.config/charlimane
 
-cp polybar ~/.config
-cp charlimane ~/.config
+cp -r polybar ~/.config
+cp -r charlimane ~/.config
 
 cp config ~/.config/i3
 
 rm -r ~/backgrounds
 
-cp backgrounds ~
+cp -r backgrounds ~
 
 cp .bashrc ~
 
 rm -r ~/.mozilla/firefox/*e/chrome
 cp -r ~/.mozilla/firefox/*e/chrome
 
-mv keybinds.conf ~/.config/hypr/custom
+cp keybinds.conf ~/.config/hypr/custom

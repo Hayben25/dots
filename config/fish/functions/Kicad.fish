418e8c9b9d891042c313f282233607edaacc41c4
Hayben25 <hayden@cianessi.com>
2025-09-18 11:07:57 -0500

    added waybar

---
+++ config/fish/functions/Kicad.fish
@@ -0,0 +1,4 @@
+function Kicad --wraps='GTK_THEME=Adwaita:dark kicad' --description 'alias Kicad=GTK_THEME=Adwaita:dark kicad'
+  GTK_THEME=Adwaita:dark kicad $argv
+        
+end
---
+++ config/fish/functions/obsidian.fish
@@ -0,0 +1,4 @@
+function obsidian --wraps=./home/hayden/code/Obsidian-1.9.12.AppImage --wraps='cd /home/hayden/code/&& ./Obsidian-1.9.12.AppImage' --description 'alias obsidian=cd /home/hayden/code/&& ./Obsidian-1.9.12.AppImage'
+  cd /home/hayden/code/&& ./Obsidian-1.9.12.AppImage $argv
+        
+end

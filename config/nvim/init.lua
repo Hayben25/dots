afd3497ffab42468e362f0203dff4d51996f770e
Hayben25 <hayden@cianessi.com>
2025-09-25 00:07:53 -0500

    updated for omf and nvim fixes (stupid tabs don't belong in nvim)

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,2 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+require('solarized').setup {
+  variant = 'summer' -- "spring" | "summer" | "autumn" | "winter" (default)
+}
